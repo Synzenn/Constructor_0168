d593bfb44ed6ced892bfb7e87241c2aea45f9813
Synzenn <velriel42@gmail.com>
2024-05-14 15:16:13 +0700

    membuat set id dan print all

--- PemdasPart1/PemdasPart1.cpp
+++ PemdasPart1/PemdasPart1.cpp
@@ -18,7 +18,15 @@ public:
 
 int Mahasiswa::nim = 168;
 
+void Mahasiswa::setID() {
+	id = ++nim;
+}
 
+void Mahasiswa::printAll() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
 
 int main() {
 
