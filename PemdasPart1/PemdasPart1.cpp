27b4191c29a2ec7cce609720036b549dd2842397
Synzenn <velriel42@gmail.com>
2024-05-14 15:15:27 +0700

    menyelesaikan class mahasiswa

--- PemdasPart1/PemdasPart1.cpp
+++ PemdasPart1/PemdasPart1.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 class Mahasiswa {
 public:
-	
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
 };
 
 
